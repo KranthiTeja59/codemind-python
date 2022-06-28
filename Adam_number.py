f9bafef9df374ef60bd120f98e5571ffefee8464
KranthiTeja59 <96741610+KranthiTeja59@users.noreply.github.com>
2022-06-28 07:06:19 +0530

    N=int(input()) sq=N**2 rev=0 while N:     rev=rev*10+N%10     N//=10 sqr=0 s=rev**2 while s:     sqr=sqr*10+s%10     s//=10 if sqr==sq:     print('True') else:     print('False')

--- Adam_number.py
+++ Adam_number.py
@@ -1,14 +1,15 @@
-def rev(n):
-    rev=0
-    while n:
-        r=n%10
-        rev=rev*10+r
-        n//=10
-    return rev
 N=int(input())
-sqo=N*N
-sqr=rev(N)*rev(N)
-if sqo==rev(sqr):
+sq=N**2
+rev=0
+while N:
+    rev=rev*10+N%10
+    N//=10
+sqr=0
+s=rev**2
+while s:
+    sqr=sqr*10+s%10
+    s//=10
+if sqr==sq:
     print('True')
 else:
     print('False')
