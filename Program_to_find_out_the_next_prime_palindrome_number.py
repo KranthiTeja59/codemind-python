0153aff26e74d4b244329c482c17c60f40c99383
KranthiTeja59 <96741610+KranthiTeja59@users.noreply.github.com>
2022-06-30 14:33:04 +0530

    def prime(n):     if n==1:         return 0     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     return 1 a=int(input()) b=a+1 while True:     if prime(b):         n=str(b)         x=n[::-1]         if x==n:             if prime(int(x)):                 print(x)                 break     b+=1

---
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -0,0 +1,19 @@
+def prime(n):
+    if n==1:
+        return 0
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return 0
+    return 1
+a=int(input())
+b=a+1
+while True:
+    if prime(b):
+        n=str(b)
+        x=n[::-1]
+        if x==n:
+            if prime(int(x)):
+                print(x)
+                break
+    b+=1
+            
