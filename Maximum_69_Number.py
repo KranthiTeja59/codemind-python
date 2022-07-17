5aad443fe7a15306b185d18482235464d550ed0c
KranthiTeja59 <96741610+KranthiTeja59@users.noreply.github.com>
2022-07-17 15:40:37 +0530

    n=int(input()) l=n%10 n//=10 t=n%10 n//=10 s=n%10 n//=10 f=n%10 if f==6:     f=9 elif s==6:     s=9 elif t==6:     t=9 else:     l=9 print(f,s,t,l,sep='')

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,4 +1,17 @@
-def max(self,num:int):
-    return str(num).replace('6','9',1)
 n=int(input())
-print(max(1,n))
+l=n%10
+n//=10
+t=n%10
+n//=10
+s=n%10
+n//=10
+f=n%10
+if f==6:
+    f=9
+elif s==6:
+    s=9
+elif t==6:
+    t=9
+else:
+    l=9
+print(f,s,t,l,sep='')
